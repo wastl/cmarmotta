369f6b377cd6ef4853aea2128b533b7181eec9d1
Sebastian Schaffert <sschaffert@apache.org>
2015-11-15 20:30:25 +0100

    gflags for server

--- service/server.cc
+++ service/server.cc
@@ -1,28 +1,30 @@
 //
 // Created by wastl on 15.11.15.
 //
-#include "persistence.h"
+#include <gflags/gflags.h>
 
-#include <grpc++/support/sync_stream.h>
+#include "persistence.h"
 
 using grpc::Status;
 using grpc::Server;
 using grpc::ServerBuilder;
 
-int main(int argc, const char** argv) {
-    std::string server_address("0.0.0.0:10000");
-    if (argc > 1) {
-        server_address = argv[1];
-    }
 
-    marmotta::persistence::LevelDBService service("/tmp/testdb", 100 * 1048576);
+DEFINE_string(host, "0.0.0.0", "Address/name of server to access.");
+DEFINE_string(port, "10000", "Port of server to access.");
+DEFINE_string(db, "/tmp/testdb", "Path to database. Will be created if non-existant.");
+
+int main(int argc, char** argv) {
+    google::ParseCommandLineFlags(&argc, &argv, true);
+
+    marmotta::persistence::LevelDBService service(FLAGS_db, 100 * 1048576);
 
     ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.AddListeningPort(FLAGS_host + ":" + FLAGS_port, grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
 
     std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Persistence Server listening on " << server_address << std::endl;
+    std::cout << "Persistence Server listening on " << FLAGS_host << ":" << FLAGS_port << std::endl;
 
     server->Wait();
 
